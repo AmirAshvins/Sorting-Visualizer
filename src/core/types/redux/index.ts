6212b044cd4e1791832c1f76fcbe4b7b0930fa75
Amirali Askari <askariamirali99@gmail.com>
2022-12-11 16:15:30 -0800

    Update the styles of the appbar and add the css for the background color of the content-rects

--- src/core/types/redux/index.ts
+++
@@ -1,7 +0,0 @@
-import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
-import ReduxStore from "../../../ui/redux";
-
-export type RootReduxState = ReturnType<typeof ReduxStore.getState>;
-export type AppDispatch = typeof ReduxStore.dispatch;
-export const useAppDispatch: () => AppDispatch = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootReduxState> = useSelector;
